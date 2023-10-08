7727fd29d927842c19771aadc6b9756ff2630ae6
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2023-10-09 03:17:41 +0500

    changed

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const toggle = document.getElementById("toogleDark");
 const body = document.querySelector("body");
 
 //toogle part
+
 toggle.addEventListener("click", function () {
   this.classList.toggle("bi-moon");
-  if (this.classList.toggle("bi bi-brightness-high")) {
-    body.style.backgroundColor = "red";
+  if (this.classList.contains("bi-moon")) {
+    body.style.backgroundColor = "white";
     body.style.color = "black";
-    body.style.transition = "2s";
+    body.style.trasition = "2s";
   } else {
     body.style.backgroundColor = "black";
     body.style.color = "white";
-    body.style.transition = "2s";
+    body.style.trasition = "2s";
   }
 });
